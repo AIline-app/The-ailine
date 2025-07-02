4ec835cf254b0aa35d110cb099eb9d42f8861bc9
Имя ФÐКирилл Мансуров <kirill.mansurov.2002@mail.ru>
2025-07-02 09:28:25 +0300

    last version

---
+++ backend/entrypoint.sh
@@ -0,0 +1,6 @@
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+#python manage.py run_import
+python manage.py collectstatic --no-input --clear
+#cp -r /app/collected_static/. /backend_static/static/
+gunicorn AstSmartTime.wsgi:application --bind 0:8000
