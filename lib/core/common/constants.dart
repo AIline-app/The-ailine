fc0809eb56ab1047eca06b47a39b013f02ed95d3
Daulet <126412542+Bastau-Bot@users.noreply.github.com>
2025-07-19 11:50:46 +0500

    minor changes

---
+++ lib/core/common/constants.dart
@@ -0,0 +1,12 @@
+// TODO:
+
+const NOT_FOUND_IMAGE = '';
+// TODO:
+const NO_IMAGE_AVAILABLE = '';
+
+class AuthConstants {
+  static const String tokenKey = 'auth_token';
+  static const String userIdKey = 'user_id';
+  static const String isFirstLaunchKey = 'is_first_launch';
+  // static const EdgeInsets defaultPadding = EdgeInsets.all(20);
+}
