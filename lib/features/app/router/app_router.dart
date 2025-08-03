f9efdb128964612b48cc5697934ce95c5670396a
Daulet <126412542+Bastau-Bot@users.noreply.github.com>
2025-08-03 12:23:18 +0500

    fix(router): set set initialLocation to /client

---
+++ lib/features/app/router/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:go_router/go_router.dart';
+import 'auth_routes.dart';
+import 'client_routes.dart';
+import 'admin_routes.dart';
+import 'director_routes.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(
+    initialLocation: '/client',
+    routes: [
+      ...authRoutes,
+      ...clientRoutes,
+      ...adminRoutes,
+      ...directorRoutes,
+    ],
+  );
+}
