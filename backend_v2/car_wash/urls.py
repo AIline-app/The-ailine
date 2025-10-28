166c0944357240994c1de7134aadfe61c9efeacb
Egor Sorokin <sorokinegor2014@gmail.com>
2025-10-28 14:47:18 +0300

    Fix url

--- backend_v2/car_wash/urls.py
+++ backend_v2/car_wash/urls.py
@@ -2,21 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from api.car_wash.views import CarWashViewSet, CarViewSet, BoxViewSet
+from iLine.constants import UUID_REGEX
 
 router = DefaultRouter()
+router.register(fr"carwash/(?P<carwash_id>{UUID_REGEX})/box", BoxViewSet, basename="car-wash-boxes")
 router.register(r'carwash', CarWashViewSet, basename='car-wash')
 router.register(r"user/car", CarViewSet, basename="user-car")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('carwash/<uuid:carwash_id>/boxes/', BoxViewSet.as_view({
-        'get': 'list',
-        'post': 'create'
-    }), name='car-wash-boxes-list'),
-    path('carwash/<uuid:carwash_id>/boxes/<uuid:box_id>/', BoxViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    }), name='car-wash-boxes-detail'),
 ]
